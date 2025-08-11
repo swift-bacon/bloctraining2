84635d7d7ae2a4e0f8d78c0c6718ba70493b6b18
Marcin Pietrzak <pietrzakmarcin@hotmail.com>
2025-08-11 15:53:15 +0200

    configure project

---
+++ lib/application.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Application extends StatelessWidget {
+    const Application({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            home: Scaffold(),
+        );
+    }
+}
